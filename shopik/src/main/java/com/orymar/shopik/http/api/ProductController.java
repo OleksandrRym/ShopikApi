35038a6e56858e34284ad764eb5565b12834c7c9
Oleksandr <145048428+OleksandrRym@users.noreply.github.com>
2026-02-01 00:06:03 +0200

    fix mapping

--- shopik/src/main/java/com/orymar/shopik/http/api/ProductController.java
+++ shopik/src/main/java/com/orymar/shopik/http/api/ProductController.java
@@ -5,12 +5,11 @@ import com.orymar.shopik.http.mapper.ProductMapper;
 import com.orymar.shopik.http.request.ProductCreateRequest;
 import com.orymar.shopik.http.request.ProductUpdateRequest;
 import com.orymar.shopik.service.ProductService;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.UUID;
 
 // ToDO: create exceptions handler
 
@@ -19,33 +18,33 @@ import java.util.UUID;
 @RequestMapping("/api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
-  private final ProductService productServiceImpl;
+  private final ProductService productService;
   private final ProductMapper mapper;
 
   @PostMapping
-  public ResponseEntity<ProductDto> create(@RequestParam ProductCreateRequest request) {
-    var product = productServiceImpl.create(request);
+  public ResponseEntity<ProductDto> create(@RequestBody ProductCreateRequest request) {
+    var product = productService.create(request);
     var dto = mapper.toDto(product);
     return ResponseEntity.ok(dto);
   }
 
   @PostMapping("/{id}")
-  public ResponseEntity<ProductDto> update(@PathVariable UUID id, @RequestParam ProductUpdateRequest request) {
-    var product = productServiceImpl.update(id, request);
+  public ResponseEntity<ProductDto> update(@PathVariable UUID id, @RequestBody ProductUpdateRequest request) {
+    var product = productService.update(id, request);
     var dto = mapper.toDto(product);
     return ResponseEntity.ok(dto);
   }
 
   @GetMapping("/{id}")
   public ResponseEntity<ProductDto> get(@PathVariable UUID id) {
-    var product = productServiceImpl.getById(id);
+    var product = productService.getById(id);
     var dto = mapper.toDto(product);
     return ResponseEntity.ok(dto);
   }
 
   @DeleteMapping("/{id}")
   public ResponseEntity delete(@PathVariable UUID id) {
-    productServiceImpl.delete(id);
+    productService.delete(id);
     return ResponseEntity.ok().build();
   }
 }
--- shopik/src/main/java/com/orymar/shopik/http/dto/ProductDto.java
+++ shopik/src/main/java/com/orymar/shopik/http/dto/ProductDto.java
@@ -1,5 +1,6 @@
 package com.orymar.shopik.http.dto;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
-public record ProductDto(Long id, String name, BigDecimal price, String description) {}
+public record ProductDto(UUID id, String name, BigDecimal price, String description) {}
