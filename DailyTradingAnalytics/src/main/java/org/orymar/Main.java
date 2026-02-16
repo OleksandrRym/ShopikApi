1b6d965d99486288bc8c22d5665471ea0c0cdf9d
Oleksandr <145048428+OleksandrRym@users.noreply.github.com>
2026-02-16 18:38:25 +0200

    cl

---
+++ DailyTradingAnalytics/src/main/java/org/orymar/Main.java
@@ -0,0 +1,10 @@
+package org.orymar;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Runtime.getRuntime().totalMemory());
+        System.out.println(Runtime.getRuntime().freeMemory());
+
+    }
+}
